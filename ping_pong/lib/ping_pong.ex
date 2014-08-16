d4647106e60563576462bffc1907bb0574aa3576
Trevor Brown <tbrown@voalte.com>
2014-08-16 16:59:08 -0400

    Added some useful demo functions to PingPong module.

--- ping_pong/lib/ping_pong.ex
+++ ping_pong/lib/ping_pong.ex
@@ -1,2 +1,24 @@
 defmodule PingPong do
+  @moduledoc """
+  For experimenting with Ping and Pong modules
+  """
+
+  @doc """
+  spawn a ping and a pong, and have them send messages back and forth
+  """
+  def start do
+    ping = spawn_link(Ping, :start, [])
+    pong = spawn_link(Pong, :start, [])
+    send ping, {:pong, pong}
+  end
+
+  @doc """
+  Spawns a Pong process and then sends three pings
+  """
+  def send_pings do
+    pong = spawn_link(Pong, :start, [])
+    send pong, {:ping, self}
+    send pong, {:ping, self}
+    send pong, {:ping, self}
+  end
 end
