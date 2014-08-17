55ed465aa4300486e85ccb0a25542d3c62364f97
Trevor Brown <tbrown@voalte.com>
2014-08-17 14:44:07 -0400

    Completed first version of the ProcessChain demo.

--- process_chain/lib/process_chain.ex
+++ process_chain/lib/process_chain.ex
@@ -1,8 +1,9 @@
 defmodule ProcessChain do
   def start(processes, loops, message) do
-    controller = spawn(ChainController, :start, [loops, message])
+    controller = spawn_link(ChainController, :start, [loops, message])
     first_pid = create_loop(processes, controller)
     send controller, {:start, first_pid}
+    :ok
   end
 
   def create_loop(0, previous_pid) do
@@ -10,7 +11,7 @@ defmodule ProcessChain do
   end
 
   def create_loop(processes, previous_pid) do
-    pid = spawn(ChainLink, :start, [previous_pid])
+    pid = spawn_link(ChainLink, :start, [previous_pid])
     create_loop(processes - 1, pid)
   end
 end
