befb889cee60850e76d552cb51d732dc8118b129
Trevor Brown <tbrown@voalte.com>
2014-08-16 21:18:59 -0400

    Added code to create the process chain to the ProcessChain module.

--- process_chain/lib/process_chain.ex
+++ process_chain/lib/process_chain.ex
@@ -1,2 +1,16 @@
 defmodule ProcessChain do
+  def start(processes, loops, message) do
+    controller = spawn(ChainController, :start, [loops, message])
+    first_pid = create_loop(processes, controller)
+    send controller, {:start, first_pid}
+  end
+
+  def create_loop(0, previous_pid) do
+    previous_pid
+  end
+
+  def create_loop(processes, previous_pid) do
+    pid = spawn(ChainLink, :start, [previous_pid])
+    create_loop(processes - 1, pid)
+  end
 end
