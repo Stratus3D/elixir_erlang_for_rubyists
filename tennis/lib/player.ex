7ad4c99d0243341218c75aa48746db60bc2a5e0c
Trevor Brown <tbrown@voalte.com>
2014-08-29 21:58:55 -0400

    Updated Player module so there is half a second delay in responses.

--- tennis/lib/player.ex
+++ tennis/lib/player.ex
@@ -6,18 +6,21 @@ defmodule Player do
   def loop(skill, name) do
     receive do
       {:service, opponent} ->
-        send opponent, {:shot, {:ball, skill}, self, name}
+        Process.send_after opponent, {:shot, {:ball, skill}, self, name}, 500
         IO.puts "#{prefix(name)} Serving ball with #{skill} spin"
         loop(skill, name)
       {:shot, {:ball, spin}, opponent, opponent_name} ->
         case Skill.calculate_return(skill, spin) do
           0 ->
-            IO.puts "#{prefix(name)} Missed ball. #{opponent_name} wins!!!"
+            Process.send_after opponent, {:over, self, name}, 500
+            IO.puts "#{prefix(name)} Missed ball"
           return_spin ->
-            send opponent, {:shot, {:ball, return_spin}, self, name}
+            Process.send_after opponent, {:shot, {:ball, return_spin}, self, name}, 500
             IO.puts "#{prefix(name)} Returned ball to #{opponent_name} with #{return_spin} spin"
             loop(skill, name)
         end
+      {:over, opponent, opponent_name} ->
+        IO.puts "#{prefix(name)} #{opponent_name} failed to return the ball. I win!!!"
     end
   end
 
