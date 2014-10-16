448a5ed4c4732c7b9eb9210305992b4ebf0c1b32
Trevor Brown <tbrown@voalte.com>
2014-10-16 16:43:08 -0400

    Updated the tests for the PingPong app.

--- ping_pong/test/ping_test.exs
+++ ping_pong/test/ping_test.exs
@@ -2,7 +2,7 @@ defmodule PingTest do
   use ExUnit.Case
 
   test "it responds to a pong with a ping" do
-    ping = spawn_link(Ping, :start, [])
+    ping = spawn_link(Ping, :loop, [])
     send ping, {:pong, self}
     assert_receive {:ping, ^ping}
     send ping, {:pong, self}
--- ping_pong/test/pong_test.exs
+++ ping_pong/test/pong_test.exs
@@ -2,7 +2,7 @@ defmodule PongTest do
   use ExUnit.Case
 
   test "it responds to a pong with a ping" do
-    pong = spawn_link(Pong, :start, [])
+    pong = spawn_link(Pong, :loop, [])
     send pong, {:ping, self}
     assert_receive {:pong, ^pong}
     send pong, {:ping, self}
